4c4c7ea79c1890a69ddba76ed8195c8d915b4b20
LAPTOP-EJFJDD3L\pasin <2018cs164@stu.ucsc.cmb.ac.lk>
2021-09-13 23:09:33 +0530

    line in

---
+++ secound.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class secound{
+	public static void main(String args[])
+	{
+		System.out.print("Enter Your Name: ");
+		Scanner sun = new Scanner(System.in);
+		System.out.println("My name is "+sun.nextLine());
+	}
+
+}
